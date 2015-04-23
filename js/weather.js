32faae89ba3ba1c21ea874f264a4cfce66624111
kevan1747 <kevan1747@gmail.com>
2015-04-23 18:42:31 -0400

    weather api

---
+++ js/weather.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+	var condition = "rainy";
+	navigator.geolocation.getCurrentPosition(showPosition);			//Get the current location coordinates.
+	function showPosition(position){
+		//Pass the coordinates to the API using the API key, and retrieve the weather data.
+		$.get("http://api.openweathermap.org/data/2.5/weather?lat=" + position.coords.latitude + "&lon=" + position.coords.longitude + "&APPID=9a25400d6a728870af915c8c614e77d7", function(data){
+			$.each(data.weather, function(i, obj){
+				if(obj.id >= 800 && obj.id <= 803){  //weather ids of sunny/cloudy, not raining. Displays rain images
+					condition = "sunny";
+				}
+				else{ //weather ids of rainy/stormy. Displays sunny images
+					document.getElementById("sm_img1").src = "img/Sunny/sunny_chime.jpg";
+					document.getElementById("sm_img2").src = "img/Sunny/sunny_bee.jpg";
+					document.getElementById("sm_img3").src = "img/Sunny/sunny_fountain.jpg";
+					document.getElementById("tracks").style.backgroundImage = "url('img/Sunny/sunny_treetops.jpg')";
+					document.body.style.backgroundImage = "url('img/Sunny/sunny_background.jpg')";
+					condition = "rainy";
+				}
+			});
+			if((data.main.temp-'273').toFixed(0) > 21){    //warm weather hotter than 70 F. Displays cold images
+				document.getElementById("sm_img1").src = "img/Cold/cold_icicle.jpg";
+				document.getElementById("sm_img2").src = "img/Cold/cold_owl.jpg";
+				document.getElementById("sm_img3").src = "img/Cold/cold_bells.jpg";
+				document.getElementById("tracks").style.backgroundImage = "url('img/Cold/cold_stream.jpg')";
+				document.body.style.backgroundImage = "url('img/Cold/cold_background.jpg')";
+				condition = "warm";
+			}
+			if((data.main.temp-'273').toFixed(0) < 0){    //cold weather colder than 32 F. Displays hot images
+				document.getElementById("sm_img1").src = "img/Warm/warm_seagulls.jpg";
+				document.getElementById("sm_img2").src = "img/Warm/warm_beach.jpg";
+				document.getElementById("sm_img3").src = "img/Warm/warm_underwater.jpg";
+				document.getElementById("tracks").style.backgroundImage = "url('img/Warm/warm_bonfire.jpg')";
+				document.body.style.backgroundImage = "url('img/Warm/warm_background.jpg')";
+				condition = "cold";
+			}
+		});
+	}
+	return condition;
+});
